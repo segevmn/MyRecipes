adb51b0448539c996246044373ef598829e00658
yarden_adika <yadika14@gmail.com>
2022-04-24 18:56:32 +0300

    lab 5 done

--- Lab 5/app/src/main/java/com/example/ex5x/FragB.java
+++ Lab 5/app/src/main/java/com/example/ex5x/FragB.java
@@ -4,6 +4,8 @@ package com.example.ex5x;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -11,19 +13,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.SeekBar;
 
-public class FragB extends Fragment /*implements SeekBar.OnSeekBarChangeListener*/ {
-	private TextView Result;
+public class FragB extends Fragment implements SeekBar.OnSeekBarChangeListener {
+	private TextView Result, example;
 	private FragBListener listener;
 	private SeekBar seekbar;
 	float resF;
+	String resS;
 
 	@Override
 	public void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-		//seekbar.setProgress(0);
-		//seekbar.setOnSeekBarChangeListener(this);
 	}
 
 	@Override
@@ -39,7 +40,6 @@ public class FragB extends Fragment /*implements SeekBar.OnSeekBarChangeListener
 		super.onAttach(context);
 	}
 
-	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		return inflater.inflate(R.layout.frag_b, container,false);
 	}
@@ -48,48 +48,58 @@ public class FragB extends Fragment /*implements SeekBar.OnSeekBarChangeListener
 	public void onViewCreated(View view, Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
 		this.Result = (TextView) view.findViewById(R.id.TV_equal);
-		//this.seekbar = (SeekBar) view.findViewById(R.id.seekBar);
+		this.example = (TextView) view.findViewById(R.id.TV_seekBar);
+		this.seekbar = (SeekBar) view.findViewById(R.id.seekBar);
+		seekbar.setProgress(0);
+		seekbar.setOnSeekBarChangeListener((SeekBar.OnSeekBarChangeListener) this);
 	}
 
 	//the activity informs fragB about new click in fragA
 	public void display(float input1, float input2, char op, float res) {
-		Result.setText(String.valueOf(input1) + " " + op + " " + String.valueOf(input2) + " " + "=" + " " + String.valueOf(res));
+		resF = res;
+		resS = String.valueOf(input1) + " " + op + " " + String.valueOf(input2) + " " + "=" + " " + String.format("%.0f", resF);
+		Result.setText(resS);
+		resS = String.valueOf(input1) + " " + op + " " + String.valueOf(input2) + " " + "=" + " ";
 	}
 
 	public interface FragBListener {
 
 	}
 
-	/*public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+	public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 		switch (progress) {
 			case 0:
-				Result.setText(String.format("%.0f", resF));
+				Result.setText(resS + String.format("%.0f", resF));
+				example.setText("Example:123");
 				break;
 			case 1:
-				Result.setText(String.format("%.1f", resF));
+				Result.setText(resS + String.format("%.1f", resF));
+				example.setText("Example:123.0");
 				break;
 			case 2:
-				Result.setText(String.format("%.2f", resF));
+				Result.setText(resS + String.format("%.2f", resF));
+				example.setText("Example:123.00");
 				break;
 			case 3:
-				Result.setText(String.format("%.3f", resF));
+				Result.setText(resS + String.format("%.3f", resF));
+				example.setText("Example:123.000");
 				break;
 			case 4:
-				Result.setText(String.format("%.4f", resF));
+				Result.setText(resS + String.format("%.4f", resF));
+				example.setText("Example:123.0000");
 				break;
 			case 5:
-				Result.setText(String.format("%.5f", resF));
+				Result.setText(resS + String.format("%.5f", resF));
+				example.setText("Example:123.00000");
 				break;
 		}
 	}
 
-	@Override
 	public void onStartTrackingTouch(SeekBar seekBar) {
 
 	}
 
-	@Override
 	public void onStopTrackingTouch(SeekBar seekBar) {
 
-	}*/
+	}
 }

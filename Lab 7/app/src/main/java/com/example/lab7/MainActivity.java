6fd65403e607ed25cd1facd73f07784afa7e189c
yarden_adika <yadika14@gmail.com>
2022-05-24 18:11:54 +0300

    lab 8

---
+++ Lab 7/app/src/main/java/com/example/lab7/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.lab7;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements CountryFragment.CountryListener {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

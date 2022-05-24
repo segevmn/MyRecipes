677bf67f70fda0d1ae49c021af0db9a21ed39524
segevmn <57412459+segevmn@users.noreply.github.com>
2022-05-24 13:20:03 +0300

    lab7 final

--- Lab7/app/src/main/java/com/example/lab7/CountryAdapter.java
+++ Lab7/app/src/main/java/com/example/lab7/CountryAdapter.java
@@ -1,8 +1,11 @@
 package com.example.lab7;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -20,22 +23,45 @@ public class CountryAdapter extends RecyclerView.Adapter<CountryAdapter.ViewHold
 
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View viewCountry = inflater.inflate(R.layout.row_item,parent,false);
+        return new ViewHolder(viewCountry);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        Country country = allcountries.get(position);
+        holder.countryName.setText(country.getName());
+        holder.countryPopulation.setText(country.getShorty());
+        holder.countryFlag.setImageResource(context.getResources().getIdentifier(country.flag,"drawable",context.getPackageName()));
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                allcountries.remove(country);
+                notifyDataSetChanged();
+                return true;
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return this.allcountries.size();
     }
     public class  ViewHolder extends  RecyclerView.ViewHolder{
+        public TextView countryName;
+        public TextView countryPopulation;
+        public ImageView countryFlag;
+        View itemView;
+
 
         public ViewHolder(@NonNull View itemView) {
+
             super(itemView);
+            this.itemView = itemView;
+            countryName = (TextView) itemView.findViewById(R.id.countryName);
+            countryPopulation = (TextView) itemView.findViewById(R.id.countryPopulation);
+            countryFlag = (ImageView) itemView.findViewById(R.id.flag);
         }
     }
 }

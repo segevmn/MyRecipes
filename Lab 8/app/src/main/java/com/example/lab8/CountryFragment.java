24ff2efaa3aa77acbb396001fb02493b3f346451
yarden_adika <yadika14@gmail.com>
2022-06-07 15:28:24 +0300

    lab 8 - done

--- Lab 8/app/src/main/java/com/example/lab8/CountryFragment.java
+++ Lab 8/app/src/main/java/com/example/lab8/CountryFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -12,6 +14,16 @@ import androidx.recyclerview.widget.RecyclerView;
 public class CountryFragment extends Fragment  {
     private RecyclerView CountriesecyclerView;
     private CountryAdapter CAdapter;
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
 
     @Override
     public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {

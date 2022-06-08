7a26145505a6516e2937b0d843d5d78dfc5115a6
segevmn <57412459+segevmn@users.noreply.github.com>
2022-06-08 22:54:28 +0300

    lab9 update

--- Lab 9/app/src/main/java/com/example/lab9/CountryAdapter.java
+++ Lab 9/app/src/main/java/com/example/lab9/CountryAdapter.java
@@ -15,15 +15,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.nio.ByteBuffer;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Map;
 
 public class CountryAdapter extends RecyclerView.Adapter<CountryAdapter.ViewHolder> {
     ArrayList<Country> allcountries;
@@ -31,11 +30,12 @@ public class CountryAdapter extends RecyclerView.Adapter<CountryAdapter.ViewHold
     private static ArrayList<String> removed_countries;
     private ArrayList<Country> countriesTemp;
     public static AdapterListener adapterListener;
+    private static String file_path;
+    public static final String FileName="remove_countries.txt";
     SharedPreferences prefManager;
     SharedPreferences.Editor editor;
-    FileOutputStream FileOutput;
-    FileInputStream FileInput;
-    String FileName = "";
+    OutputStream FileOutput;
+    InputStream FileInput;
 
     public CountryAdapter(AdapterListener adapterListener, Context context, ArrayList<Country> allcountries) {
         this.adapterListener = adapterListener;
@@ -43,49 +43,77 @@ public class CountryAdapter extends RecyclerView.Adapter<CountryAdapter.ViewHold
         editor = prefManager.edit();
         this.context = context;
         this.allcountries = allcountries;
+
     }
 
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         View viewCountry = inflater.inflate(R.layout.row_item,parent,false);
+        file_path = context.getFilesDir().getAbsolutePath();
         countriesTemp = new ArrayList<Country>();
+        StringBuilder text =null;
         String line;
+        File file = new File(file_path, File.separator+FileName);
         boolean remember = prefManager.getBoolean("rememberCB", false);
-        removed_countries = new ArrayList<>();
+
 
         //raw-file
         try {
-            FileInput = context.openFileInput(String.valueOf(FileInput));
-            ByteBuffer bf = ByteBuffer.allocate(500);
+            if(!file.exists()) {
+                file.createNewFile();
+            }
+            FileInput = new FileInputStream(file);
+            text = new StringBuilder();
             InputStreamReader inputStreamReader = new InputStreamReader(FileInput);
             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
             while ((line = bufferedReader.readLine()) != null) {
-                removed_countries.add(line);
+                text.append(line);
+                text.append('\n');
             }
             FileInput.close();
             inputStreamReader.close();
+            bufferedReader.close();}catch (IOException e) {
+            e.printStackTrace();
+        }
+
+            //removed_countries = new ArrayList<String>(Arrays.asList(text.toString().split("\n")));
+
+           removed_countries = new ArrayList<>();
+            for(Map.Entry<String,?> entry : prefManager.getAll().entrySet()){
+                if(entry.getValue() instanceof String){
+                    removed_countries.add(String.valueOf(entry.getValue()));
+                }
+            }
 
             if(remember == true) {
                 for(Country country : allcountries) {
-                    if(!removed_countries.contains(country)) {
-                        FileInput.read(bf.array());
+                    if(!removed_countries.contains(country.getName())) {
                         countriesTemp.add(country);
                     }
                 }
-                allcountries.addAll(countriesTemp);
+               allcountries = countriesTemp;
             }
             else {
-                FileOutput.write(("".getBytes()));
-                FileOutput.close();
-                removed_countries.clear();
+                try {
+                    FileOutput = new FileOutputStream(file_path + File.separator + FileName);
+                    FileOutput.write(("".getBytes()));
+                    FileOutput.close();
+                    removed_countries.clear();
+                }catch (IOException e) {
+                    e.printStackTrace();
+                }
+                //end-raw-file
+
+                //shared-preferences
+                editor = prefManager.edit();
+                editor.clear();
+                editor.commit();
+                //end-shared-preferences
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
+
         //end-raw-file
 
         return new ViewHolder(viewCountry);
@@ -108,17 +136,31 @@ public class CountryAdapter extends RecyclerView.Adapter<CountryAdapter.ViewHold
             @Override
             public boolean onLongClick(View view) {
                 //raw-file
-                try {
-                    FileOutput = context.openFileOutput(FileName, context.MODE_PRIVATE);
-                    ByteBuffer bf = ByteBuffer.allocate(500);
-                    OutputStreamWriter outputWriter = new OutputStreamWriter(FileOutput);
-                    outputWriter.write(countryPos.getName().getBytes()+"\n");
+                File dir = new File(file_path);
+                if(!dir.exists()){
+                    dir.mkdir();
+                }
+                File file =new File(file_path, File.separator+FileName);
+               try {
+                   if(!file.exists()) {
+                       file.createNewFile();
+                   }
+                   FileOutputStream FileOut = new FileOutputStream(file, true);
+                    OutputStreamWriter outputWriter = new OutputStreamWriter(FileOut);
+                    outputWriter.write(countryPos.getName()+"\n");
                     outputWriter.close();
                     removed_countries.add(countryPos.getName());
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
                 //end-raw-file
+
+                //shared-preferences
+               SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString(countryPos.getName(),countryPos.getName());
+                editor.commit();
+                //end-shared-preferences
 
                 allcountries.remove(countryPos);
                 notifyDataSetChanged();
